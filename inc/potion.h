55ab773c5afb2af73ce780ce5f006bb5cf4512bd
Carter Aitken <soupyninja64@gmail.com>
2025-07-22 14:42:59 -0400

    finished potion

--- inc/potion.h
+++ inc/potion.h
@@ -1,6 +1,8 @@
 #ifndef __potion__
 #define __potion__
 
+#include "item.h"
+
 class Potion: public Item {
     public:
     Potion(const Entity::EntityImpl &data); 
@@ -9,11 +11,45 @@ class Potion: public Item {
     virtual void affect(Entity&) override;
 };
 
-class HeathPotion: public Potion {
+class HealthPotion: public Potion {
     public:
     HealthPotion(const Entity::EntityImpl &data);
     virtual ~HealthPotion() = default;
-    virtual char icon() const override;
+    virtual void affect(Entity&) override;
+};
+
+class AttackPotion: public Potion {
+    public:
+    AttackPotion(const Entity::EntityImpl &data);
+    virtual ~AttackPotion() = default;
+    virtual void affect(Entity&) override;
+};
+
+class DefensePotion: public Potion {
+    public:
+    DefensePotion(const Entity::EntityImpl &data);
+    virtual ~DefensePotion() = default;
+    virtual void affect(Entity&) override;
+};
+
+class PoisonPotion: public Potion {
+    public:
+    PoisonPotion(const Entity::EntityImpl &data);
+    virtual ~PoisonPotion() = default;
+    virtual void affect(Entity&) override;
+};
+
+class WeakPotion: public Potion {
+    public:
+    WeakPotion(const Entity::EntityImpl &data);
+    virtual ~WeakPotion() = default;
+    virtual void affect(Entity&) override;
+};
+
+class BrittlePotion: public Potion {
+    public:
+    BrittlePotion(const Entity::EntityImpl &data);
+    virtual ~BrittlePotion() = default;
     virtual void affect(Entity&) override;
 };
 
--- src/potion.cc
+++ src/potion.cc
@@ -1,29 +1,90 @@
 #include "potion.h"
 
+#include <iostream>
+
 #include "util.h"
+#include "character.h"
+
+using namespace std;
 
 Potion::Potion(const Entity::EntityImpl &data):
     Item{data} {}
 
-HealthPotion(const Entity::EntityImpl &data):
+HealthPotion::HealthPotion(const Entity::EntityImpl &data):
+    Potion{data} {}
+
+AttackPotion::AttackPotion(const Entity::EntityImpl &data):
+    Potion{data} {}
+
+DefensePotion::DefensePotion(const Entity::EntityImpl &data):
+    Potion{data} {}
+
+PoisonPotion::PoisonPotion(const Entity::EntityImpl &data):
+    Potion{data} {}
+    
+WeakPotion::WeakPotion(const Entity::EntityImpl &data):
+    Potion{data} {}
+     
+BrittlePotion::BrittlePotion(const Entity::EntityImpl &data):
     Potion{data} {}
 
 char Potion::icon() const {
     return 'P';
 }
 
-char HealthPotion::icon() const {
-    return 'H';
-}
 
-void Potion::affect(Entity &e) override {
+void Potion::affect(Entity &) {
     getCout() << "THE POTION DOES NOTHING!!!!" << endl;
+    getCout() << "I SHOULDN'T EXIST." << endl;
 }
 
-void HealthPotion::affect(Entity &e) override {
+void HealthPotion::affect(Entity &e) {
     try {
         Character &character {dynamic_cast<Character&>(e)};
         character.setHP(character.getHP() + 10);
     }
     catch(const bad_cast &ex) {}
 }
+
+void AttackPotion::affect(Entity &e) {
+    try {
+        Character &character {dynamic_cast<Character&>(e)};
+        character.setATK(character.getATK() + 5);
+    }
+    catch(const bad_cast &ex) {}
+}
+
+void DefensePotion::affect(Entity &e) {
+    try {
+        Character &character {dynamic_cast<Character&>(e)};
+        character.setDEF(character.getDEF() + 5);
+    }
+    catch(const bad_cast &ex) {}
+}
+
+void PoisonPotion::affect(Entity &e) {
+    try {
+        Character &character {dynamic_cast<Character&>(e)};
+        character.setHP(max(1, character.getHP() - 10));
+    }
+    catch(const bad_cast &ex) {}
+}
+
+void WeakPotion::affect(Entity &e) {
+    try {
+        Character &character {dynamic_cast<Character&>(e)};
+        character.setATK(max(0, character.getATK() - 5));
+    }
+    catch(const bad_cast &ex) {}
+}
+
+
+void WeakPotion::affect(Entity &e) {
+    try {
+        Character &character {dynamic_cast<Character&>(e)};
+        character.setDEF(max(0, character.getATK() - 5));
+    }
+    catch(const bad_cast &ex) {}
+}
+
+// character.setHP(max(1, character.getHP() - 10));
